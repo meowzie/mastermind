41ec60349d7c17659798a61f014944b220e7acd7
meowzie <appenge2@gmail.com>
2022-01-09 15:38:11 +0200

    Renamed 'Game' module to 'Common'; Moved 'Common' module out of the Game class; Moved 'color_picker' method from MasterCode class to Common module; Created 'initialize' method for the Game class; Changed names of @compu_numbers and @playa_numbers in the MasterCode and Guess classes to @numbers in each class; Removed redundant 'number_adder' method from Guess class; Added functionality to the 'take_colors' method to allow it to push colors the the @sequence array and to add each color's corresponding number to the @numbers array; Created 'play' method in Game class, which plays the game; Created 'total_success' method, 'partial_ordered_success' method, and 'partial_unordered_success' method

--- mastermind.rb
+++ mastermind.rb
@@ -2,66 +2,101 @@
 
 require 'rainbow'
 
+# contains message and common methods
+module Common
+  MESSAGE = "Guess the sequence of 4 colors. Enter #{Rainbow('R').red} for #{Rainbow('red').red}, "\
+  "#{Rainbow('B').blue} for #{Rainbow('blue').blue}, #{Rainbow('G').green} for #{Rainbow('green').green}, or "\
+  "#{Rainbow('Y').yellow} for #{Rainbow('yellow').yellow}"
+
+  def color_picker(sequence, num)
+    case num
+    when 1
+      sequence.push(Rainbow('•').red)
+    when 2
+      sequence.push(Rainbow('•').blue)
+    when 3
+      sequence.push(Rainbow('•').green)
+    when 4
+      sequence.push(Rainbow('•').yellow)
+    end
+  end
+end
+
+# creates the game
 class Game
-  # contains message
-  module Game
-    MESSAGE = "Guess the sequence of 4 colors. Enter #{Rainbow('R').red} for #{Rainbow('red').red}, "\
-    "#{Rainbow('B').blue} for #{Rainbow('blue').blue}, #{Rainbow('G').green} for #{Rainbow('green').green}, or "\
-    "#{Rainbow('Y').yellow} for #{Rainbow('yellow').yellow}"
+  attr_accessor :master, :guess, :ordered_common, :unordered_common
+
+  def initialize
+    @master = MasterCode.new
+    @guess = Guess.new
+    @ordered_common = []
+    @unordered_common = []
   end
 
-  # creates a sequence of colors
+  # contains gameplay
+  def play
+    @master.frice
+    @guess.take_colors
+  end
+
+  # checks if the player has won
+  def total_success?
+    @master.numbers == @guess.numbers
+  end
+
+  # checks if the player has guessed a correct color in the correct spot
+  def partial_ordered_success
+    0.upto(3) do |number|
+      @ordered_common.push(number) if @master.numbers[number] == @guess.numbers[number]
+    end
+    @ordered_common
+  end
+
+  # checks if the player has guessed a correct color
+  def partial_unordered_success
+    mutable_numbers = @master.numbers
+    0.upto(3) do |number|
+      if mutable_numbers.include?(@guess.numbers[number]) && @ordered_common.none? { |num| num == number }
+        mutable_numbers.delete(@guess.numbers[number])
+        @unordered_common.push(number)
+      end
+    end
+    @unordered_common
+  end
+
+  # creates the 'master code' sequence of colors
   class MasterCode
-    attr_accessor :sequence, :compu_numbers
+    include Common
+    attr_accessor :sequence, :numbers
 
     def initialize
       @sequence = []
-      @compu_numbers = []
+      @numbers = []
     end
 
-    def color_picker(sequence, num)
-      case num
-      when 1
-        sequence.push(Rainbow('•').red)
-      when 2
-        sequence.push(Rainbow('•').blue)
-      when 3
-        sequence.push(Rainbow('•').green)
-      when 4
-        sequence.push(Rainbow('•').yellow)
-      end
-    end
-
+    # saves the number that a color corresponds to in the @numbers array & converts each number into a color to be
+    # stored in the @sequence array
     def color_saver(sequence, num, numbers)
       numbers.push(num)
       color_picker(sequence, num)
     end
 
     def frice
-      4.times { color_saver(@sequence, rand(1..4), @compu_numbers) }
-      @sequence.join
+      4.times { color_saver(@sequence, rand(1..4), @numbers) }
     end
   end
 
-  # creates the player's guesses
+  # creates the player's color sequence guesses
   class Guess
-    include Game
-    attr_accessor :playa_numbers, :sequence
+    include Common
+    attr_accessor :numbers, :sequence
 
     def initialize
       @sequence = nil
-      @playa_numbers = []
+      @numbers = []
     end
 
-    def take_colors
-      puts MESSAGE
-
-      @sequence = gets.chomp.upcase.split('')
-      allowed = %w[R B G Y]
-
-      @sequence.all? { |letter| allowed.include?(letter) } && @sequence.length == 4 ? @sequence : 'Invalid input'
-    end
-
+    # converts the player's letter guesses into numbers
     def letter_to_num(letter)
       case letter
       when 'R'
@@ -74,9 +109,19 @@ class Game
         4
       end
     end
+  end
 
-    def number_adder
-      @playa_numbers = @sequence.map { |letter| self.letter_to_num(letter) }
+  # allows the player to guess a sequence; invalid input detection built-in
+  def take_colors
+    puts MESSAGE
+    @sequence = gets.chomp.upcase.split('')
+    allowed = %w[R B G Y]
+    if @sequence.all? { |letter| allowed.include?(letter) } && @sequence.length == 4
+      @numbers = @sequence.map { |letter| self.letter_to_num(letter) }
+      @sequence = []
+      @numbers.map { |number| color_picker(@sequence, number) }
+    else
+      'Invalid input'
     end
   end
 end
